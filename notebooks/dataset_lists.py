d405b3c2060e78d16ae98e4ab2714a77ad99c872
Andy Barrett <apbarret@nsidc.org>
2023-08-09 18:48:18 +0000

    initial commit of dataset lists

---
+++ notebooks/dataset_lists.py
@@ -0,0 +1,22 @@
+"""Collections of datasets for benchmarhing"""
+
+BEAM_GROUP = "gt1l/land_ice_segments"
+
+# Datasets to read for benchmark tests
+ONE_BEAM_GROUP = [
+    f"{BEAM_GROUP}/atl06_quality_summary", 
+    f"{BEAM_GROUP}/delta_time",
+    f"{BEAM_GROUP}/h_li",
+    f"{BEAM_GROUP}/h_li_sigma",
+    f"{BEAM_GROUP}/latitude",
+    f"{BEAM_GROUP}/longitude",
+    f"{BEAM_GROUP}/segment_id",
+    f"{BEAM_GROUP}/sigma_geo_h",
+]
+
+XARRAY_LIKE_ONE_DATASET = [
+    f"{BEAM_GROUP}/delta_time",
+    f"{BEAM_GROUP}/h_li",
+    f"{BEAM_GROUP}/latitude",
+    f"{BEAM_GROUP}/longitude",
+]
