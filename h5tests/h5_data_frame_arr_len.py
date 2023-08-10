c57e36f0264e825622adb7f23b65aece0d26e295
Default user <aimee@developmentseed.org>
2023-08-10 21:27:44 +0000

    Add H5DataFrame to h5tets

---
+++ h5tests/h5_data_frame_arr_len.py
@@ -0,0 +1,25 @@
+from .h5test import H5Test, timer_decorator
+import h5py
+import pandas as pd
+import subprocess
+
+try:
+    from gedi_subset.h5frame import H5DataFrame
+except ImportError:
+    completed_process = subprocess.run([
+        'pip', 'install', 'git+https://github.com/MAAP-Project/gedi-subsetter.git@0.6.0'
+    ])
+    from gedi_subset.h5frame import H5DataFrame
+
+class H5DataFrameArrLen(H5Test):
+    @timer_decorator
+    def run(self):
+        group = '/gt1l/heights'
+        variable = 'h_ph'        
+        dataframes = []
+        for file in self.files:
+            with h5py.File(name=self.s3_fs.open(file, 'rb')) as h5:
+                df = H5DataFrame(h5[f"{group[1:]}"])
+                dataframes.append(df[variable])
+        final_dataframe: pd.Series = pd.concat(objs=dataframes, axis="index")
+        return len(final_dataframe)
