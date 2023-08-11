2dd20687bc623dd67ec878eb51aaecc8983cdf1d
Andy Barrett <apbarret@nsidc.org>
2023-08-11 16:47:26 +0000

    add test class for subset and geospatial helpers

--- h5tests/h5py_arr_subset.py
+++ h5tests/h5py_arr_subset.py
@@ -1,16 +1,23 @@
+import os
+import sys
+
 from .h5test import H5Test, timer_decorator
 import h5py
 import numpy as np
 
+current = os.path.abspath('..')
+sys.path.append(current)
+from helpers.geospatial import get_subset_region, get_subset_indices
+
 class H5pyArrSubset(H5Test):
     
-    def __init__(self, geometry=None):
+    def __init__(self, data_format, geometry=None):
         """
         geometry : path to geojson file containing geometry
                    **Could be list containing [lonmin, lonmax, latmin, latmax]**
         """
-        super().__init__()
-        self.bounds = get_bounds(geometry)
+        super().__init__(data_format)
+        self.bounds = get_subset_region(geometry)
         
     @timer_decorator
     def run(self):
@@ -19,9 +26,20 @@ class H5pyArrSubset(H5Test):
         group = '/gt1l/heights'
         variable = 'h_ph'        
         for file in self.files:
-            s3_filename = f"s3://{self.bucket}/{file}"
-            with h5py.File(self.s3_fs.open(s3_filename, 'rb')) as f:
-                data = f[f'{group}/{variable}'][:]
+            with h5py.File(self.s3_fs.open(file, 'rb')) as f:
+                
+                lat = f[f'{group}/lat_ph'][:]
+                lon = f[f'{group}/lon_ph'][:]
+        
+                idx_start, idx_end = get_subset_indices(lat, lon, self.bounds)
+                
+                # Leaving this code here so that we can create a DataFrame or
+                # Dataset at a later date.  Suggest creating dict which can be 
+                # passsed to xarray or (geo)pandas
+                # lat[idx_start:idx_end])
+                # lon[idx_start:idx_end])
+
+                data = f[f'{group}/{variable}'][idx_start:idx_end]
                 # Need to test if using concatenate is faster
                 final_h5py_array = np.insert(
                     final_h5py_array,
