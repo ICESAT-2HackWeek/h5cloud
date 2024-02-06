a5fe2e281fcf84156d76a8ffbfd112acb428ad0e
betolink <betolin@gmail.com>
2024-02-05 19:57:25 -0600

    h5coro needs some upstream changes to work with annon=True access

--- h5tests/h5coro_arr_mean.py
+++ h5tests/h5coro_arr_mean.py
@@ -1,4 +1,4 @@
-from .h5test import H5Test, timer_decorator
+from h5test import H5Test, timer_decorator
 import numpy as np
 import subprocess
 
@@ -12,15 +12,22 @@ except:
 
 from h5coro import h5coro, s3driver, filedriver
 h5coro.config(errorChecking=True, verbose=False, enableAttributes=False)
+
+driver =  s3driver.S3Driver
     
 class H5CoroArrMean(H5Test):
     @timer_decorator
-    def run(self):
-        group = '/gt1l/heights'
-        variable = 'h_ph'        
+    def run(self, dataset="/gt1l/heights", variable="h_ph"):
+        group = dataset
+        variable = variable     
         final_h5coro_array = []
+        if self.files[0].startswith("s3://cryo"):
+            credentials = {}
+        else:
+            credentials = {"region_name": "us-west-2",
+                           "anon": True}
         for file in self.files:
-            h5obj = h5coro.H5Coro(file.replace("s3://", ""), s3driver.S3Driver)
+            h5obj = h5coro.H5Coro(file.replace("s3://", ""), s3driver.S3Driver, credentials=credentials)
             output = h5obj.readDatasets(datasets=[f'{group}/{variable}'], block=True)
             data = h5obj[f'{group}/{variable}'].values
             final_h5coro_array = np.insert(final_h5coro_array, len(final_h5coro_array), data, axis=None)
